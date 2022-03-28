5d3d2d0f6558a97d9e8da10fc63bbf3cbc5dc1a5
CreatedCell <mdgamer1324@gmail.com>
2022-03-28 00:09:41 -0400

    More stuff
    mhm

---
+++ signup-submit.php
@@ -0,0 +1,22 @@
+<html>
+	
+	<head>
+		<title>Do you want money?</title>
+		<link rel="stylesheet" href="questions.css">
+		<meta charset="utf-8" />
+		
+	</head>
+
+	<body>
+        <div class = "container">
+        <?php	
+		echo '<p>Welcome to Who Wants To Be Slightly  Richer, ' . $_POST['name'] . '!</p>';
+		?>
+
+        <br><br>
+        <a href="wwtbsr.html">Join us at the home page.</a>
+        </div>
+			
+			</body>
+
+</html>
